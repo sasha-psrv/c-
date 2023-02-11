c18458bfb85945218c8c01682707bbcc613f2c16
Sasha <pisarevasas@yandex.ru>
2023-02-11 16:35:02 +0300

    ex2

---
+++ ex2/Program.cs
@@ -0,0 +1,11 @@
+﻿// 2. Написать программу вычисления произведения чисел от 1 до N
+
+Console.Clear();
+Console.WriteLine("введите число: ");
+int N = int.Parse(Console.ReadLine()!);
+int res = 1;
+for (int i = 1; i <= N; i++)
+{
+    res *= i;
+}
+Console.WriteLine(res);
