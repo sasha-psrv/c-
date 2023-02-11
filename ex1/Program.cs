5793fa0863fcab73a6a9581df4587c5b853ce386
Sasha <pisarevasas@yandex.ru>
2023-02-11 16:27:41 +0300

    ex1

---
+++ ex1/Program.cs
@@ -0,0 +1,10 @@
+﻿// 1.Подсчитать сумму цифр в числе
+Console.Clear();
+Console.WriteLine("введите число: ");
+int N = int.Parse(Console.ReadLine()!);
+int res = 0;
+for (int i = 0; i <= N; i++)
+{
+    res += i;
+}
+Console.WriteLine(res);
