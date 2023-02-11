e10720c2d6d60ad4dbb23d0e277cb66be59e5081
Sasha <pisarevasas@yandex.ru>
2023-02-11 16:50:48 +0300

    ex3

---
+++ ex3/Program.cs
@@ -0,0 +1,10 @@
+﻿// 3. Показать кубы чисел, заканчивающихся на четную цифру
+
+Console.Clear();
+Console.WriteLine("введите число: ");
+int N = int.Parse(Console.ReadLine()!);
+if (N%2==0){
+    Console.WriteLine(N*N*N);
+}else{
+    Console.WriteLine("не четное");
+}
