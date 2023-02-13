84736b5840e25dd6f85f1ff7e84c114e28e26ade
Sasha <pisarevasas@yandex.ru>
2023-02-13 23:09:11 +0300

    ex36

---
+++ ex36/Program.cs
@@ -0,0 +1,35 @@
+﻿// Задача 36: Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+
+// [3, 7, 23, 12] -> 19
+
+// [-4, -6, 89, 6] -> 0
+
+int[] GetArray(int size, int minValue, int maxValue)
+{
+    int[] result = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        result[i] = new Random().Next(minValue, maxValue + 1);
+    }
+    return result;
+}
+
+int summ(int[] inArray)
+{
+    int count = 0;
+    for (int i = 0; i < inArray.Length; i++)
+    {
+        if (i % 2 != 0)
+        {
+            count += inArray[i];
+        }
+    }
+    return count;
+}
+
+
+
+int[] array = GetArray(5, -100, 100);
+Console.WriteLine(String.Join(", ", array));
+int count = summ(array);
+Console.WriteLine(count);
