22ab511ba889df004098a697e1bc459ea0e92bf8
Sasha <pisarevasas@yandex.ru>
2023-02-17 22:24:34 +0300

    ex41

---
+++ ex41/Program.cs
@@ -0,0 +1,34 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел.
+// Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// -1, -7, 567, 89, 223-> 3
+
+
+int[] GetArray(int size, int minValue, int maxValue)
+{
+    int[] result = new int[size];
+
+    for (int i = 0; i < size; i++)
+    {
+        result[i] = new Random().Next(minValue, maxValue + 1);
+    }
+    return result;
+}
+
+int Result(int[] inArray)
+{
+    int count = 0;
+    for (int i = 0; i < inArray.Length; i++)
+    {
+        if (inArray[i] > 0)
+        {
+            count++;
+        }
+    }
+    return count;
+}
+
+int[] array = GetArray(5,-100,100);
+Console.WriteLine(String.Join(", ", array));
+int count = Result(array);
+Console.WriteLine(count);
