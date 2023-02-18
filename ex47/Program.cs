ec2fae7b069316d2e0a0633ffcda2b1b687e5983
Sasha <pisarevasas@yandex.ru>
2023-02-18 20:39:33 +0300

    ex47

---
+++ ex47/Program.cs
@@ -0,0 +1,40 @@
+﻿// Задача 47: Задайте двумерный массив размером m×n,
+// заполненный случайными вещественными числами.
+// m = 3, n = 4.
+// 0,5 7 -2 -0,2
+// 1 -3,3 8 -9,9
+// 8 7,8 -7,1 9
+
+double[,] GetArray(int m, int n, double minValue, double maxValue)
+{
+    double[,] result = new double[m, n];
+
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            result[i, j] = Math.Round(new Random().NextDouble() * (maxValue - minValue) + minValue, 2);
+        }
+    }
+    return result;
+}
+
+void PrintArray(double[,] inArray)
+{
+    for (int i = 0; i < inArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inArray.GetLength(1); j++)
+        {
+            Console.Write($"{inArray[i, j]}\t ");
+        }
+        Console.WriteLine();
+    }
+
+}
+Console.WriteLine("введите кол-во строк: ");
+int row = int.Parse(Console.ReadLine()!);
+Console.WriteLine("введите кол-во столбцов: ");
+int col = int.Parse(Console.ReadLine()!);
+
+double[,] array2D = GetArray(row, col, -100, 100);
+PrintArray(array2D);
